ca5f4aab10d22867168ab6da2c19659486d3c8e4
ShiyuCheng2018 <starkeyshiyu@gmail.com>
2022-06-26 00:15:15 -0700

    [feat] server thread exit

--- user_srv/server.py
+++ user_srv/server.py
@@ -1,11 +1,21 @@
 import logging
+import os
+import signal
+import sys
 from concurrent import futures
 
 import grpc
 from loguru import logger
 
+BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, BASE_DIR)
 from proto import user_pb2_grpc
 from user_srv.handler.user import UserServicer
+
+
+def on_exit(signo, frame):
+    logger.info("Terminating Thread...")
+    sys.exit(0)
 
 
 def serve():
@@ -13,6 +23,11 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     user_pb2_grpc.add_UserServicer_to_server(UserServicer(), server)
     server.add_insecure_port('[::]:50051')
+
+    # signal disconnection for main thread
+    signal.signal(signal.SIGINT, on_exit)
+    signal.signal(signal.SIGTERM, on_exit)
+
     logger.info(f"Sever started: 127.0.0.1:50051")
     server.start()
     server.wait_for_termination()
